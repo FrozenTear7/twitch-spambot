7ae8159fca210ecf78bc686841e8eb620e53caa7
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-07-17 18:37:03 +0200

    Check for sub emotes and add README

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # twitch-spambot
-An automated twitch spam user
+
+### An automated twitch spammer
+
+## Idea
+
+The idea of this small project is to process the messages from given channel
+and simply spam whichever phrase is currently the most popular.
+
+We can achieve this by constantly monitoring the chat with IRC from Twitch's
+Node.js library [tmi.js](https://github.com/tmijs).
+
+We also fetch sub emotes from that channel to reject messages containing them,
+if we're not subbed.
+
+## How to run the program
+
+_Node.js_ is required, the program was created with _v14.4.0_.
+
+```bash
+npm install
+npm start
+```
+
+_.env_ file is also required to provide data for the api and the config.
+Create an _.env_ file consiting of values as shown below:
+
+```bash
+USERNAME=<username>
+CLIENT_TOKEN=<client_token>
+CHANNEL_NAME=<channel_name>
+CHANNEL_ID=<channel_id>
+SUBMODE=false/true
+```
+
+To get Client ID and secret, you have to create an application
+on the Twitch Developer site.
+Client token can be retrieved from https://twitchapps.com/tmi/.
+
+To get Channel ID, you have to make a request to _https://api.twitch.tv/kraken/users?login=forsen_ with Headers:
+
+- Client-ID: <client_id>
+- Accept: application/vnd.twitchtv.v5+json
+
+You can also adjust the config at the top of the _index.js_ file to your liking.
+
+Example config:
+
+```js
+const readInterval = 5000 // in [ms]
+const similarityThreshold = 0.8
+const repetitionThreshold = 3
+```
--- index.js
+++ index.js
@@ -1,75 +1,101 @@
 import dotenv from 'dotenv'
 import tmi from 'tmi.js'
-import { Worker } from 'worker_threads'
 import stringSimilarity from 'string-similarity'
+import request from 'request'
 
 dotenv.config({ silent: true })
 
-const readInterval = 5000 // in [ms]
+const readInterval = 3000 // in [ms]
 const similarityThreshold = 0.8
-const repetitionThreshold = 3
+const repetitionThreshold = 5
 
+let prevMsg = null
 let currentMsgDict = {}
+let channelSubEmotes = []
 
 const opts = {
   identity: {
-    username: process.env.CHANNEL_NAME,
+    username: process.env.USERNAME,
     password: process.env.CLIENT_TOKEN,
   },
-  // channels: [process.env.CHANNEL_NAME],
-  channels: ['forsen'],
+  channels: [process.env.CHANNEL_NAME],
 }
 
 const client = new tmi.client(opts)
 
 const produceSpam = async () => {
-  console.log('Interval started')
+  // During the wait we gather messages to the dictionary
   await new Promise((resolve) => setTimeout(resolve, readInterval))
-  console.log('Interval passed')
 
   let mostPopularSpam = null
 
   if (currentMsgDict !== {})
     mostPopularSpam = Object.entries(currentMsgDict)
-      .filter((entry) => entry[1] > repetitionThreshold)
+      .filter((entry) => entry[1] >= repetitionThreshold)
       .sort((a, b) => b[1] - a[1])[0]
-  console.log(mostPopularSpam)
+
+  if (mostPopularSpam && mostPopularSpam[0] !== prevMsg) {
+    client.say(process.env.USERNAME, mostPopularSpam[0])
+    prevMsg = mostPopularSpam[0]
+  }
 
   currentMsgDict = {}
-  produceSpam()
+
+  produceSpam() // The spam never ends
 }
 
 const incrementOrAdd = (msg) => {
-  let similarityDict = {}
-
   const dictKeys = Object.keys(currentMsgDict)
   let bestMatch = ''
 
   if (dictKeys.length > 0)
     bestMatch = stringSimilarity.findBestMatch(msg, dictKeys).bestMatch
+
+  // If there is a match similar enough increment the value
   if (bestMatch.target && bestMatch.rating >= similarityThreshold)
     currentMsgDict[bestMatch.target] += 1
+  // Else just create an entry
   else currentMsgDict[msg] = 1
 }
 
+// Pass every received message to the parser
 const onMessageHandler = (target, context, msg, self) => {
   if (self) {
     return
   }
 
-  const currentTimestamp = context['tmi-sent-ts']
+  if (!process.env.SUBMODE) {
+    const msgWords = msg.split(' ')
+    const subEmotesIntersection = msgWords.filter((word) =>
+      channelSubEmotes.includes(word)
+    )
 
-  incrementOrAdd(msg.toString())
-  // client.say(process.env.CHANNEL_NAME, 'bruh')
+    // We reject messages containing sub emotes
+    if (subEmotesIntersection.length === 0) incrementOrAdd(msg)
+  } else {
+    incrementOrAdd(msg)
+  }
 }
 
+// Start the spam once connected
 const onConnectedHandler = (addr, port) => {
   console.log(`* Connected to ${addr}:${port}`)
   console.log('* Starting the spambot')
   produceSpam()
 }
 
+// Register handlers
 client.on('message', onMessageHandler)
 client.on('connected', onConnectedHandler)
 
+// Fetch sub emotes
+request(
+  `https://api.twitchemotes.com/api/v4/channels/${process.env.CHANNEL_ID}`,
+  (error, response, body) => {
+    channelSubEmotes = JSON.parse(body).emotes.map((emote) => emote.code)
+    console.log(channelSubEmotes)
+  }
+)
+
+// Start the client
 client.connect()
