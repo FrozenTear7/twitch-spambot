af64dc53eb9839f1a725893c3395d587c97cfed9
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-07-17 17:09:59 +0200

    Find the currently most popular spam in chat

--- index.js
+++ index.js
@@ -1,42 +1,75 @@
 import dotenv from 'dotenv'
 import tmi from 'tmi.js'
+import { Worker } from 'worker_threads'
+import stringSimilarity from 'string-similarity'
 
 dotenv.config({ silent: true })
+
+const readInterval = 5000 // in [ms]
+const similarityThreshold = 0.8
+const repetitionThreshold = 3
+
+let currentMsgDict = {}
 
 const opts = {
   identity: {
     username: process.env.CHANNEL_NAME,
     password: process.env.CLIENT_TOKEN,
   },
-  channels: [process.env.CHANNEL_NAME],
+  // channels: [process.env.CHANNEL_NAME],
+  channels: ['forsen'],
 }
 
 const client = new tmi.client(opts)
+
+const produceSpam = async () => {
+  console.log('Interval started')
+  await new Promise((resolve) => setTimeout(resolve, readInterval))
+  console.log('Interval passed')
+
+  let mostPopularSpam = null
+
+  if (currentMsgDict !== {})
+    mostPopularSpam = Object.entries(currentMsgDict)
+      .filter((entry) => entry[1] > repetitionThreshold)
+      .sort((a, b) => b[1] - a[1])[0]
+  console.log(mostPopularSpam)
+
+  currentMsgDict = {}
+  produceSpam()
+}
+
+const incrementOrAdd = (msg) => {
+  let similarityDict = {}
+
+  const dictKeys = Object.keys(currentMsgDict)
+  let bestMatch = ''
+
+  if (dictKeys.length > 0)
+    bestMatch = stringSimilarity.findBestMatch(msg, dictKeys).bestMatch
+  if (bestMatch.target && bestMatch.rating >= similarityThreshold)
+    currentMsgDict[bestMatch.target] += 1
+  else currentMsgDict[msg] = 1
+}
+
+const onMessageHandler = (target, context, msg, self) => {
+  if (self) {
+    return
+  }
+
+  const currentTimestamp = context['tmi-sent-ts']
+
+  incrementOrAdd(msg.toString())
+  // client.say(process.env.CHANNEL_NAME, 'bruh')
+}
+
+const onConnectedHandler = (addr, port) => {
+  console.log(`* Connected to ${addr}:${port}`)
+  console.log('* Starting the spambot')
+  produceSpam()
+}
 
 client.on('message', onMessageHandler)
 client.on('connected', onConnectedHandler)
 
 client.connect()
-
-// Called every time a message comes in
-function onMessageHandler(target, context, msg, self) {
-  if (self) {
-    return
-  } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim()
-
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice()
-    client.say(target, `You rolled a ${num}`)
-    console.log(`* Executed ${commandName} command`)
-  } else {
-    console.log(`* Unknown command ${commandName}`)
-  }
-}
-
-function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`)
-}
