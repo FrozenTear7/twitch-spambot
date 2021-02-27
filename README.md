71198ed11a3b1df3daa18b31bd921fa3422dcb30
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-02-27 21:01:13 +0100

    Add coverage badges

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ![Automated by Buddy](https://assets.buddy.works/automated-dark.svg)
 
 [![buddy pipeline](https://app.buddy.works/frozentear7/twitch-spambot/pipelines/pipeline/312651/badge.svg?token=67c90bad49816f04938626247f8b89bb1bbf782b583b5fc4b78a863b0d4d18df 'buddy pipeline')](https://app.buddy.works/frozentear7/twitch-spambot/pipelines/pipeline/312651)
+![Coverage](./badges/badge-statements.svg)
 
 ![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/FrozenTear7/twitch-spambot)
 ![GitHub Release Date](https://img.shields.io/github/release-date/FrozenTear7/twitch-spambot)
--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
   // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: ['**/*.{ts}'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -34,12 +34,7 @@ export default {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ['json-summary', 'text', 'lcov', 'clover'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
